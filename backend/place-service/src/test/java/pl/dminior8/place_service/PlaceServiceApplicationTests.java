a37829f900a70f52229511d4b57efc2a5c72dd3f
Daniel <daniel.minior.pk@gmail.com>
2025-01-28 22:41:52 +0100

    Init commit

---
+++ backend/place-service/src/test/java/pl/dminior8/place_service/PlaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dminior8.place_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
